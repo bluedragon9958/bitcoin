fed6568a67c85c457dca9dd7c08cc6732a0dbde8
Chad Russell <chadr76@gmail.com>
2018-04-01 23:17:37 -0700

    -removing asserts

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -234,8 +234,8 @@ public:
         printf("%s\n", consensus.hashGenesisBlock.ToString());
         printf("%s\n", genesis.hashMerkleRoot.ToString());
 
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+//        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
+//        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         vFixedSeeds.clear();
         vSeeds.clear();

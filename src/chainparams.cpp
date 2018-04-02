5d04e71bb567a22b303b8c567ffdbf8d45878bad
Chad Russell <chadr76@gmail.com>
2018-04-01 23:36:47 -0700

    -removing genesis crap

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -232,36 +232,36 @@ public:
 
  // calculate Genesis Block
         // Reset genesis
-        consensus.hashGenesisBlock = uint256S("0x");
-        std::cout << std::string("Begin calculating Testnet Genesis Block:\n");
-        if (true && (genesis.GetHash(consensus) != consensus.hashGenesisBlock)) {
-//                     LogPrintf("Calculating Mainnet Genesis Block:\n");
-            arith_uint256 hashTarget = arith_uint256().SetCompact(genesis.nBits);
-            uint256 hash;
-            genesis.nNonce = ArithToUint256(0);
-            while (UintToArith256(genesis.GetHash(consensus)) > hashTarget)
-            {
-                genesis.nNonce = ArithToUint256(UintToArith256(genesis.nNonce) + 1);
-                         if (genesis.nNonce == ArithToUint256(arith_uint256(0)))
-                         {
-                             LogPrintf("NONCE WRAPPED, incrementing time");
-                             std::cout << std::string("NONCE WRAPPED, incrementing time:\n");
-                             ++genesis.nTime;
-                         }
-
-                         if ((int)genesis.nNonce.GetUint64(0) % 10000 == 0)
-                         {
-                              std::cout << strNetworkID << " hashTarget: " << hashTarget.ToString() << " nonce: " << genesis.nNonce.ToString() << " time: " << genesis.nTime << " hash: " << genesis.GetHash(consensus).ToString().c_str() << "\r";
-                         }
-
-            }
-                     std::cout << "Testnet ---\n";
-                     std::cout << "  nonce: " << genesis.nNonce.ToString() <<  "\n";
-                     std::cout << "   time: " << genesis.nTime << "\n";
-                     std::cout << "   hash: " << genesis.GetHash(consensus).ToString().c_str() << "\n";
-                     std::cout << "   merklehash: "  << genesis.hashMerkleRoot.ToString().c_str() << "\n";
-        }
-        std::cout << std::string("Finished calculating Testnet Genesis Block:\n");
+//        consensus.hashGenesisBlock = uint256S("0x");
+//        std::cout << std::string("Begin calculating Testnet Genesis Block:\n");
+//        if (true && (genesis.GetHash(consensus) != consensus.hashGenesisBlock)) {
+////                     LogPrintf("Calculating Mainnet Genesis Block:\n");
+//            arith_uint256 hashTarget = arith_uint256().SetCompact(genesis.nBits);
+//            uint256 hash;
+//            genesis.nNonce = ArithToUint256(0);
+//            while (UintToArith256(genesis.GetHash(consensus)) > hashTarget)
+//            {
+//                genesis.nNonce = ArithToUint256(UintToArith256(genesis.nNonce) + 1);
+//                         if (genesis.nNonce == ArithToUint256(arith_uint256(0)))
+//                         {
+//                             LogPrintf("NONCE WRAPPED, incrementing time");
+//                             std::cout << std::string("NONCE WRAPPED, incrementing time:\n");
+//                             ++genesis.nTime;
+//                         }
+//
+//                         if ((int)genesis.nNonce.GetUint64(0) % 10000 == 0)
+//                         {
+//                              std::cout << strNetworkID << " hashTarget: " << hashTarget.ToString() << " nonce: " << genesis.nNonce.ToString() << " time: " << genesis.nTime << " hash: " << genesis.GetHash(consensus).ToString().c_str() << "\r";
+//                         }
+//
+//            }
+//                     std::cout << "Testnet ---\n";
+//                     std::cout << "  nonce: " << genesis.nNonce.ToString() <<  "\n";
+//                     std::cout << "   time: " << genesis.nTime << "\n";
+//                     std::cout << "   hash: " << genesis.GetHash(consensus).ToString().c_str() << "\n";
+//                     std::cout << "   merklehash: "  << genesis.hashMerkleRoot.ToString().c_str() << "\n";
+//        }
+//        std::cout << std::string("Finished calculating Testnet Genesis Block:\n");
 
 //        genesis = CreateGenesisBlock(1522639490, 0, 0x1d00ffff, 1, 50 * COIN);
         genesis = CreateGenesisBlock(1519394018, 6446, 0x1f0fffff, 1, 1 * COIN);

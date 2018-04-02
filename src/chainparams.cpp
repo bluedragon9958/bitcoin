8e3f16e586eab03c9dd25d9368a936bd1ffd76f1
Chad Russell <chadr76@gmail.com>
2018-04-01 22:15:34 -0700

    -testing

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,6 +121,9 @@ public:
 
         genesis = CreateGenesisBlock(1522639525, 2083236893, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
+
+        printf("pi = %.5f", 4*atan(1.0));
+
         assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
@@ -230,6 +233,7 @@ public:
 
         genesis = CreateGenesisBlock(1522639490, 414098458, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
+        strprintf("", );
 //        assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
         assert(genesis.hashMerkleRoot == uint256S("0x"));
 

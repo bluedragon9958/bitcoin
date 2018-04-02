10c9889dfb1ae27264a2b7f1f72a2d2242fe9e45
Chad Russell <chadr76@gmail.com>
2018-04-01 23:15:32 -0700

    -more merkle work v2

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -231,8 +231,8 @@ public:
         genesis = CreateGenesisBlock(1522639490, 0, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
 
-        printf("%s\n", consensus.hashGenesisBlock.ToString().c_str());
-        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        printf("%s\n", consensus.hashGenesisBlock.ToString());
+        printf("%s\n", genesis.hashMerkleRoot.ToString());
 
         assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));

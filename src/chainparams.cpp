34bee44379154c4983e38684b4070736eddf93ea
Chad Russell <chadr76@gmail.com>
2018-04-01 22:54:13 -0700

    -2

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -5,14 +5,14 @@
 
 #include <chainparams.h>
 #include <consensus/merkle.h>
-
 #include <tinyformat.h>
 #include <util.h>
 #include <utilstrencodings.h>
-
 #include <assert.h>
-
 #include <chainparamsseeds.h>
+
+#include <iostream>   // std::cout
+#include <string>     // std::string, std::to_string
 
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
@@ -121,9 +121,6 @@ public:
 
         genesis = CreateGenesisBlock(1522639525, 2083236893, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-
-        printf("pi = %.5f", 4*atan(1.0));
-
         assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
@@ -233,8 +230,9 @@ public:
 
         genesis = CreateGenesisBlock(1522639490, 414098458, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
+        printf("pi = %.5f", 4*atan(1.0));
 //        assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
-        assert(genesis.hashMerkleRoot == uint256S("0x"));
+        assert(genesis.hashMerkleRoot == uint256S("0x32373365383362346561623332393261636362613665303664303035626439636136326336653734663462363832303136326237623434636432613262643332"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
